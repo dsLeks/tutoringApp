e53e4c73e4f4845ac91d5f44a7c7c3e6f04642f4
Justin Diones <justindiones@gmail.com>
2021-09-15 23:57:43 -0700

    worked on about css, added barebones template for individual about pages

--- application/src/components/Member1.js
+++ application/src/components/Member1.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import Picture from '../components/pictures/justin.jpg'
+import './About.css'
  
 const Member1 = () => {
     return (
-       <div>
-          <h1>Justin Diones</h1>
-          <p>information aobut the member</p>
+       <div className="container">
+         <div>
+         <h1 >Justin Diones</h1>
+          
+         <div className="about-info">
+            GitHub ID: justdio
+            <br/> <br/>
+            Front end lead of Team 7. Senior year at SFSU. Scheduled to graduate Spring 2022. 
+            Working on the front end of the team project using React in Visual Studio Code.
+         </div>
+
+         <div className="about-picture">
+            <img src={Picture} />
+         </div>
+
+         </div>
        </div>
     );
 }
