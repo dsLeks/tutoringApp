c53ca6705943110db36189798221b3ddf47bb973
Justin Diones <justindiones@gmail.com>
2021-11-11 13:01:03 -0800

    testing merge

--- application/client/src/components/Mynavbar.js
+++ application/client/src/components/Mynavbar.js
@@ -36,7 +36,7 @@ class mynavbar extends React.Component{
                                     <NavDropdown.Item href="#"> Course </NavDropdown.Item>
                                     <NavDropdown.Item href="#"> Major </NavDropdown.Item>
                                     <NavDropdown.Item href="#"> Tutors </NavDropdown.Item>
-                                    <NavDropdown.Item href="#"> Testing </NavDropdown.Item>
+                                    <NavDropdown.Item href="#"> Hello </NavDropdown.Item>
                                 </NavDropdown>
                             </Nav>
                             <div className="input-group" id="thesearch">
