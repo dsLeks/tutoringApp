425d7965ec9fba1674099193e7ee17269aaa9e07
dsLeks <alekhyahello@gmail.com>
2021-11-16 12:35:09 -0800

    made changes so that search displays appropriate results

--- application/client/src/components/Search.js
+++ application/client/src/components/Search.js
@@ -13,74 +13,77 @@ export default function Search() {
     },[history.location.state])
 
     console.log('state on search screen ', state)
+   
 
     return (
         <div id="searchComponent">
             {/*<div style={{width:'100vw'}}>*/}
             <div>
-                {(state && state.searchResponse && state.searchResponse != undefined && state.searchResponse.length > 0) ? state.searchResponse.map(searchResponse =>
-                
-                 /*<table id="tutors" className="search-table">
-                     <tr>
-                         <th>TutorID</th>
-                         <th>Email</th>
-                         <th>First Name</th>
-                         <th>Last Name</th>
-                         <th>Course</th>
-                         <th>Photo</th>
-                     </tr>
-                     <tr>
-                         <td>{searchResponse.tutorID}</td>
-                         <td>{searchResponse.email}</td> 
-                        <td>{searchResponse.firstName}</td> 
-                        <td>{searchResponse.lastName}</td> 
-                        <td>{searchResponse.courseTeaching}</td> 
-                        <td><img src={searchResponse.imageReference} width="200" height="100"></img></td>
-                     </tr>
-                </table>
-                */
-                
-                
-                   
-                       
-                <div className="swrapper">
-                    <div className="i-wrapper">
-                        <img src={searchResponse.imageReference} width="200" height="200"></img>
-                    </div>
-                    <div className="info-wrapper">
-                        <ul className="searchul">
-                            <li>TutorID: {searchResponse.tutorID}</li>
-                            
-                            <li>First Name: {searchResponse.firstName}</li>
-                                <li>Last Name: {searchResponse.lastName}</li>
-                                <li>Email: {searchResponse.email}</li>
-                            <li>Course: {searchResponse.courseTeaching}</li>
-                        
-                        
-                        
-                        
-                        
-                        </ul>
-                        </div>
-                        
+                {(state && state.searchResponse && state.searchResponse != undefined && state.searchResponse.length > 0) ? state.searchResponse.map(searchResponse => {
+                    if(Object.keys(searchResponse).length == 11) {
+                        return (
+                            <div className="swrapper">
+                            <div className="i-wrapper">
+                                <img src={searchResponse.imageReference} width="200" height="200"></img>
+                            </div>
+                            <div className="info-wrapper">
+                                <ul className="searchul">
+                                    {console.log ("Search Response length is: " + Object.keys(searchResponse).length)}
+                                    <li>TutorID: {searchResponse.tutorID}</li>
+                                    <li>First Name: {searchResponse.firstName}</li>
+                                        <li>Last Name: {searchResponse.lastName}</li>
+                                        <li>Email: {searchResponse.email}</li>
+                                    <li>Course: {searchResponse.courseTeaching}</li>
+                                    <li>Course Description: {searchResponse.courseDescription}</li>
+                                </ul>
+                                </div>
+                            </div> 
+                        )
+                    }
 
-                    </div>
-                    
-                
-                )
-            
-                    :
+                    else if(Object.keys(searchResponse).length == 6) { //for Tutors
+                        return (
+                            <div className="swrapper">
+                            <div className="i-wrapper">
+                                <img src={searchResponse.imageReference} width="200" height="200"></img>
+                            </div>
+                            <div className="info-wrapper">
+                                <ul className="searchul">
+                                    {console.log ("Search Response length is: " + Object.keys(searchResponse).length)}
+                                    <li>TutorID: {searchResponse.tutorID}</li>
+                                    <li>First Name: {searchResponse.firstName}</li>
+                                        <li>Last Name: {searchResponse.lastName}</li>
+                                        <li>Email: {searchResponse.email}</li>
+                                    <li>Course: {searchResponse.courseTeaching}</li>
+                                </ul>
+                                </div>
+                            </div> 
+                        )
+                    }
+
+                    else if(Object.keys(searchResponse).length == 5) { //for Courses
+                        return (
+                            <div className="swrapper">
+                            <div className="info-wrapper">
+                                <ul className="searchul">
+                                    {console.log ("Search Response length is: " + Object.keys(searchResponse).length)}
+                                    <li>CourseID: {searchResponse.courseID}</li>
+                                    <li>TutorID: {searchResponse.tutor}</li>
+                                        <li>Tutor Name: {searchResponse.tutorName}</li>
+                                        <li>Course Name: {searchResponse.courseName}</li>
+                                    <li>Course Description: {searchResponse.courseDescription}</li>
+                                </ul>
+                                </div>
+                            </div> 
+                        )
+                    }
+
+                }) :
                     <div style={{height: '50vh', display: 'flex', justifyContent:'center', flexDirection:'column', alignItems:'center'}}>
                         Nothing found!
-
                     </div>
-            
-            }
-             
-              
+            }    
             </div>
-            
-        </div>
-        
+        </div> 
     )
 }
