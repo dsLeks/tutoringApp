b2f7693a1f0c7d1224b58a33fd300c7a098d9428
Ben <rhuang10@mail.sfsu.edu>
2021-12-09 18:03:33 -0800

    reworked the tutor dashboard frontend to make it more align with intended purpose

--- application/client/src/components/Tutor.js
+++ application/client/src/components/Tutor.js
@@ -1,76 +1,239 @@
+/*
+File: The tutor dashboard
+Worked on by: Rui Qi Huang
+Purpose: allows tutor to view messages form students
+*/
+
 
 import './Tutor.css';
-import Picture from './pictures/math-teacher-vector.png'
-// import 'bootstrap/dist/css/bootstrap.css';
-// import 'bootstrap/dist/js/bootstrap.bundle';
-// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-// import 'bootstrap/dist/js/bootstrap.js';
 import React from "react";
 
 
 function Tutor() {
 
     return (
-        <div class="container rounded bg-white mt-5 mb-5">
 
 
-            <div class="row">
-                <div class="col-md-3 border-right">
-                    <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img className="rounded-circle img-thumbnail mt-5 border border-success" width="200px" src={Picture}></img>
-                        <span className="font-weight-bold text-lowercase">Name: Michael </span>
-                        <span className="text-lowercase text-black-50">Email: michael@gmail.com </span>
-                        <span className="text-lowercase text-black-50">Schedule: 10am-2pm Thurs-Fri </span>
+
+    <div className="container">
+        <div className="row">
+            <div className="col-md-12">
+                <div className="card">
+                    <div id="dashboard" className="card-body bg-info text-white mailbox-widget pb-0">
+                        <h2 className="text-white pb-3">Dashboard</h2>
+
                     </div>
-                </div>
-                <div class="col-md-5 border-right">
-                    <div class="p-3 py-5">
-                        <div class="d-flex justify-content-between align-items-center mb-3">
-                            <h4 class="text-right">Tutor Dashboard</h4>
-                        </div>
+                    <div className="tab-content" id="myTabContent">
+                        <div className="tab-pane fade active show" id="inbox" aria-labelledby="inbox-tab"
+                             role="tabpanel">
+                            <div>
+                                <div className="row p-4 no-gutters align-items-center">
+                                    <div className="col-sm-12 col-md-6">
+                                        <h3 className="font-light mb-0"><i className="ti-email mr-2"/>Inbox</h3>
 
+                                    </div>
+                                    <div className="col-sm-12 col-md-6">
+                                        <ul className="list-inline dl mb-0 float-left float-md-right">
+                                            <li className="list-inline-item text-info mr-3">
 
+                                            </li>
 
-
-                        <div class="row mt-2">
-                            <div class="col-md-10">Message Students
-
-                                <div >
-
-                                        <select  id="categoryDROPDOWN"
-                                                className="form-select"
-                                                name="selectedCategory"
-                                                type="category"
-                                                aria-label=".form-select-sm example">
-                                            <option selected>List of Students</option>
-                                            <option value="1">Michael</option>
-                                            <option value="2">Billy</option>
-                                            <option value="2">Johnny</option>
-                                        </select>
-
+                                        </ul>
+                                    </div>
                                 </div>
 
-                                <textarea id="messageStudent" type="text" class="form-control"  placeholder="Send Message.."/>
+                                <div className="table-responsive">
+                                    <table className="table email-table no-wrap table-hover v-middle mb-0 font-14">
+                                        <tbody>
+
+                                        <tr className="border-top">
+
+                                            <td className="pl-3">
+                                                <div className="custom-control custom-checkbox">
+                                                    <input type="checkbox" className="custom-control-input" id="cst1"/>
+                                                    <label className="custom-control-label"
+                                                           htmlFor="cst1">&nbsp;</label>
+                                                </div>
+                                            </td>
+
+                                            <td><i className="fa fa-star text-warning"></i></td>
+                                            <td>
+                                                <span className="mb-0 text-muted">Bob Shimurda</span>
+                                            </td>
+
+                                            <td>
+                                                <a className="link" href="javascript: void(0)">
+
+                                                    <span className="text-dark">but also the leap into electronic typesetting, remaining essentially unchanged. It was</span>
+                                                </a>
+                                            </td>
+
+                                            <td><i className="fa fa-paperclip text-muted"></i></td>
+
+                                            <td className="text-muted">May 13</td>
+                                        </tr>
+
+                                        <tr>
+
+                                            <td className="pl-3">
+                                                <div className="custom-control custom-checkbox">
+                                                    <input type="checkbox" className="custom-control-input" id="cst2"/>
+                                                    <label className="custom-control-label"
+                                                           htmlFor="cst2">&nbsp;</label>
+                                                </div>
+                                            </td>
+
+                                            <td><i className="fa fa-star"></i></td>
+
+                                            <td>
+                                                <span className="mb-0 text-muted">Genelia</span>
+                                            </td>
+                                            <td>
+                                                <a className="link" href="javascript: void(0)">
+
+                                                    <span className="text-dark">it has a more-or-less normal distribution of letters, as opposed to</span>
+                                                </a>
+                                            </td>
+                                            <td><i className="fa fa-paperclip text-muted"></i></td>
+                                            <td className="text-muted">January 13</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="pl-3">
+                                                <div className="custom-control custom-checkbox">
+                                                    <input type="checkbox" className="custom-control-input" id="cst3"/>
+                                                    <label className="custom-control-label"
+                                                           htmlFor="cst3">&nbsp;</label>
+                                                </div>
+                                            </td>
+                                            <td><i className="fa fa-star text-warning"></i></td>
+                                            <td className="user-name max-texts">
+                                                <span className="mb-0 text-muted font-light">Ritesh</span>
+                                            </td>
+                                            <td>
+                                                <a className="link" href="javascript: void(0)">
+
+                                                    <span className="font-light text-dark">Something..</span>
+                                                </a>
+                                            </td>
+                                            <td><i className="fa fa-paperclip text-muted"></i></td>
+                                            <td className="text-muted font-light">May 13</td>
+                                        </tr>
+                                        <tr className="">
+                                            <td className="pl-3">
+                                                <div className="custom-control custom-checkbox">
+                                                    <input type="checkbox" className="custom-control-input" id="cst4"/>
+                                                    <label className="custom-control-label"
+                                                           htmlFor="cst4">&nbsp;</label>
+                                                </div>
+                                            </td>
+                                            <td><i className="fa fa-star"></i></td>
+                                            <td>
+                                                <span className="mb-0 text-muted font-light">Kumar</span>
+                                            </td>
+                                            <td>
+                                                <a className="link" href="javascript: void(0)">
+                                                   <span
+                                                    className="font-light text-dark">Perspiciatis</span>
+                                                </a>
+                                            </td>
+                                            <td><i className="fa fa-paperclip text-muted"></i></td>
+                                            <td className="text-muted font-light">May 9</td>
+                                        </tr>
+                                        <tr className="">
+                                            <td className="pl-3">
+                                                <div className="custom-control custom-checkbox">
+                                                    <input type="checkbox" className="custom-control-input" id="cst5"/>
+                                                    <label className="custom-control-label"
+                                                           htmlFor="cst5">&nbsp;</label>
+                                                </div>
+                                            </td>
+                                            <td><i className="fa fa-star"></i></td>
+                                            <td>
+                                                <span className="mb-0 text-muted font-light">John Abraham</span>
+                                            </td>
+                                            <td>
+                                                <a className="link" href="javascript: void(0)">
+
+                                                    <span className="font-light text-dark">Lorem</span>
+                                                </a>
+                                            </td>
+                                            <td><i className="fa fa-paperclip text-muted"></i></td>
+                                            <td className="text-muted font-light">Feburary 10</td>
+                                        </tr>
+                                        <tr className="">
+                                            <td className="pl-3">
+                                                <div className="custom-control custom-checkbox">
+                                                    <input type="checkbox" className="custom-control-input" id="cst6"/>
+                                                    <label className="custom-control-label"
+                                                           htmlFor="cst6">&nbsp;</label>
+                                                </div>
+                                            </td>
+                                            <td><i className="fa fa-star text-warning"></i></td>
+                                            <td>
+                                                <span className="mb-0 text-muted font-light">Akshay</span>
+                                            </td>
+                                            <td>
+                                                <a className="link" href="javascript: void(0)">
+
+                                                    <span className="font-light text-dark">ipsum</span>
+                                                </a>
+                                            </td>
+                                            <td><i className="fa fa-paperclip text-muted"></i></td>
+                                            <td className="text-muted font-light">December 09</td>
+                                        </tr>
+                                        <tr className="">
+                                            <td className="pl-3">
+                                                <div className="custom-control custom-checkbox">
+                                                    <input type="checkbox" className="custom-control-input" id="cst7"/>
+                                                    <label className="custom-control-label"
+                                                           htmlFor="cst7">&nbsp;</label>
+                                                </div>
+                                            </td>
+                                            <td><i className="fa fa-star text-warning"></i></td>
+                                            <td>
+                                                <span className="mb-0 text-muted font-light">Hanna</span>
+                                            </td>
+                                            <td>
+                                                <a className="link" href="javascript: void(0)">
+                                                <span
+                                                    className="font-light text-dark"> Unde</span>
+                                                </a>
+                                            </td>
+                                            <td><i className="fa fa-paperclip text-muted"></i></td>
+                                            <td className="text-muted font-light">Mar 09</td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
-
                         </div>
-                        <button class="btn btn-primary profile-button" type="button">Send </button> <span></span>
-                        <button className="btn btn-primary profile-button" type="button">Upload Documents</button>
+                        <div className="tab-pane fade" id="sent" aria-labelledby="sent-tab" role="tabpanel">
+                            <div className="row p-3 text-dark">
+                                <div className="col-md-6">
+                                    <h3 className="font-light">Lets check profile</h3>
+                                    <h4 className="font-light">you can use it with the small code</h4>
+                                </div>
+                                <div className="col-md-6 text-right">
+                                    <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim
+                                        justo, rhoncus ut, imperdiet a.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="tab-pane fade" id="spam" aria-labelledby="spam-tab" role="tabpanel">
+                            <div className="row p-3 text-dark">
+                                <div className="col-md-6">
+                                    <h3 className="font-light">Come on you have a lot message</h3>
+                                    <h4 className="font-light">you can use it with the small code</h4>
+                                </div>
 
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="p-3 py-5">
-                        {/*<div class="d-flex justify-content-between align-items-center experience"><span>Edit Experience</span><span class="border px-3 p-1 add-experience"><i class="fa fa-plus"></i>&nbsp;Experience</span></div>*/}
-                        {/*<div class="col-md-12"><label class="labels">Experience in Designing</label><input type="text" class="form-control" placeholder="experience" value=""/></div>*/}
-                        {/*<div class="col-md-12"><label class="labels">Additional Details</label><input type="text" class="form-control" placeholder="additional details" value=""/></div>*/}
+                            </div>
+                        </div>
 
                     </div>
                 </div>
             </div>
-
-
         </div>
-
+    </div>
 
     );
 }
