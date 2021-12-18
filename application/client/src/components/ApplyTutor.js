026b8bc67c2e6f9ac883f14405d4fb368fb37e3e
William Lushbough <lushbough@gmail.com>
2021-12-17 21:12:09 -0800

    apply tutor validation added for email and names.

--- application/client/src/components/ApplyTutor.js
+++ application/client/src/components/ApplyTutor.js
@@ -1,21 +1,26 @@
 import React from "react";
 import "./ApplyTutor.scss";
 
+const initialState = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  firstNameError: "Cannot be empty",
+  lastNameError: "Field Cannot be empty",
+  emailError: "",
+  major: "",
+  course: "",
+  description: "",
+  photo: null,
+  resume: null,
+
+}
+
 class ApplyTutor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      firstName: "",
-      lastName: "",
-      email: "",
-      firstNameError: "Cannot be empty",
-      lastNameError: "Field Cannot be empty",
-      emailError: "",
-      major: "",
-      course: "",
-      description: "",
-      photo: null,
-      resume: null,
+      initialState
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -45,26 +50,59 @@ class ApplyTutor extends React.Component {
     });
   }
 
+//Starting to validate the form
+validate = () => {
+
+if(!this.state.firstName) {
+  return false
+}
+if (!this.state.lastName) {
+  return false;
+}
+
+if (!this.state.email.includes('sfsu')) { //email must include sfsu
+this.setState({emailError: "email invalid"})
+console.log("email error has occured")
+}
+if(this.state.emailError) {
+  return false;
+}
+return true; //no problems with the form
+  };
+
   async handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData();
 
-    formData.append("firstName", this.state.firstName);
-    formData.append("lastName", this.state.lastName);
-    formData.append("email", this.state.email);
-    formData.append("major", this.state.major);
-    formData.append("course", this.state.course);
-    formData.append("description", this.state.description);
-    formData.append("resume", this.state.resume);
-    formData.append("photo", this.state.photo);
+    //Handling the validation of the form
+    const isValid = this.validate();
+    if (isValid) {
+      console.log(this.state)
 
-    const response = await fetch("/tutorapply", {
-      method: "POST",
-      body: formData,
-    });
+    //moved below code inside isValid if statement
+    //we append to formdata when the submission is Valid.
+      formData.append("firstName", this.state.firstName);
+      formData.append("lastName", this.state.lastName);
+      formData.append("email", this.state.email);
+      formData.append("major", this.state.major);
+      formData.append("course", this.state.course);
+      formData.append("description", this.state.description);
+      formData.append("resume", this.state.resume);
+      formData.append("photo", this.state.photo);
+  
+      const response = await fetch("/tutorapply", {
+        method: "POST",
+        body: formData,
+      });
+  
+      const text = await response.text();
+      console.log(text);
 
-    const text = await response.text();
-    console.log(text);
+      this.setState(initialState) //form goes back to initial state
+    }
+
+
+
   }
 
   render() {
@@ -82,6 +120,7 @@ class ApplyTutor extends React.Component {
               <input
                 name="firstName"
                 type="text"
+                placeholder="first name"
                 value={this.state.firstName}
                 onChange={this.handleInputChange}
               />
@@ -99,6 +138,7 @@ class ApplyTutor extends React.Component {
             <input
               name="lastName"
               type="text"
+              placeholder="last name"
               value={this.state.lastName}
               onChange={this.handleInputChange}
             />
@@ -112,6 +152,7 @@ class ApplyTutor extends React.Component {
             <input
               name="email"
               type="text"
+              placeholder="email"
               value={this.state.email}
               onChange={this.handleInputChange}
             />
@@ -122,6 +163,7 @@ class ApplyTutor extends React.Component {
             <input
               name="major"
               type="text"
+              placeholder="major"
               value={this.state.major}
               onChange={this.handleInputChange}
             />
@@ -134,6 +176,7 @@ class ApplyTutor extends React.Component {
             <input
               name="course"
               type="text"
+              placeholder="course"
               value={this.state.course}
               onChange={this.handleInputChange}
             />
@@ -147,6 +190,7 @@ class ApplyTutor extends React.Component {
             <input
               name="description"
               type="text"
+              placeholder="description"
               value={this.state.description}
               onChange={this.handleInputChange}
             />
