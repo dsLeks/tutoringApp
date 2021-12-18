90a508969150cc2aaf55a9696c94965747a37bf5
rupakkhatri <rkhatri@mail.sfsu.edu>
2021-12-17 18:39:41 -0800

    Updated ApplyTuto section

--- application/client/src/components/ApplyTutor.js
+++ application/client/src/components/ApplyTutor.js
@@ -1,147 +1,270 @@
-import React from 'react';
-import './ApplyTutor.css';
+import React from "react";
+import "./ApplyTutor.scss";
 
 class ApplyTutor extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          firstName: "",
-          lastName: "",
-          email: "",
-          major: "",
-          course: "",
-          description:"",
-          photo: null,
-          resume: null
-        };
-    
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleFileInputChange = this.handleFileInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
-    
-      handleInputChange(event) {
-        event.preventDefault();
-        const target = event.target;
-        this.setState((state) => {
-            return {
-                [target.name]: target.value,
-            }
-        });
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      firstNameError: "Cannot be empty",
+      lastNameError: "Field Cannot be empty",
+      emailError: "",
+      major: "",
+      course: "",
+      description: "",
+      photo: null,
+      resume: null,
+    };
 
-      handleFileInputChange(event) {
-        event.preventDefault();
-        const target = event.target;
-        const file = target.files[0];
-        console.log("Data File",file); 
-        this.setState((state) => {
-            return {
-                [target.name]: file
-            }
-        });
-      }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleFileInputChange = this.handleFileInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-      async handleSubmit(event) {
-          event.preventDefault(); 
-          const formData = new FormData();
+  handleInputChange(event) {
+    event.preventDefault();
+    const target = event.target;
+    this.setState((state) => {
+      return {
+        [target.name]: target.value,
+      };
+    });
+  }
 
-          formData.append("firstName", this.state.firstName);
-          formData.append("lastName", this.state.lastName);
-          formData.append("email", this.state.email);
-          formData.append("major", this.state.major);
-          formData.append("course", this.state.course);
-          formData.append("description", this.state.description);
-          formData.append("resume", this.state.resume);
-          formData.append("photo", this.state.photo); 
+  handleFileInputChange(event) {
+    event.preventDefault();
+    const target = event.target;
+    const file = target.files[0];
+    console.log("Data File", file);
+    this.setState((state) => {
+      return {
+        [target.name]: file,
+      };
+    });
+  }
 
-          const response = await fetch('/tutorapply', {
-              method: 'POST',
-              body: formData
-          });
+  async handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData();
 
-          const text = await response.text();
-          console.log(text); 
-      }
-    render(){
-        return (
-            <div className="tutorForm">
-                <form onSubmit={this.handleSubmit}>
-                        <label>
-                            First Name
-                            <input
-                                name="firstName"
-                            type="text"
-                                value={this.state.firstName}
-                                onChange={this.handleInputChange}
-                            />
-                        </label>
-                        <label>
-                            Last Name
-                            <input
-                                name="lastName"
-                                type="text"
-                                value={this.state.lastName}
-                                onChange={this.handleInputChange}
-                            />
-                        </label>
-                        <label>
-                            Email
-                            <input
-                                name="email"
-                                type="text"
-                                value={this.state.email}
-                                onChange={this.handleInputChange}
-                            />
-                        </label>
-                        <label>
-                            Major
-                            <input
-                                name="major"
-                                type="text"
-                                value={this.state.major}
-                                onChange={this.handleInputChange}
-                            />
-                        </label>
-                        <label>
-                            Course
-                            <input
-                                name="course"
-                                type="text"
-                                value={this.state.course}
-                                onChange={this.handleInputChange}
-                            />
-                        </label>
-                        <label>
-                            Description
-                            <input
-                                name="description"
-                                type="text"
-                                value={this.state.description}
-                                onChange={this.handleInputChange}
-                            />
-                        </label>
-                        <label>
-                            Upload Resume
-                            <input
-                                name="resume"
-                                type="file"
-                                onChange={this.handleFileInputChange}
-                            />
-                        </label>
-                        <label>
-                            Upload Photo
-                            <input
-                                name="photo"
-                                type="file"
-                                onChange={this.handleFileInputChange}
-                            />
-                        </label>
-                        <button type="submit">Register</button>
-                    </form> 
+    formData.append("firstName", this.state.firstName);
+    formData.append("lastName", this.state.lastName);
+    formData.append("email", this.state.email);
+    formData.append("major", this.state.major);
+    formData.append("course", this.state.course);
+    formData.append("description", this.state.description);
+    formData.append("resume", this.state.resume);
+    formData.append("photo", this.state.photo);
+
+    const response = await fetch("/tutorapply", {
+      method: "POST",
+      body: formData,
+    });
+
+    const text = await response.text();
+    console.log(text);
+  }
+
+  render() {
+    return (
+      <>
+        <form class="application-form" onSubmit={this.handleSubmit}>
+          <h1 class="form-heading">Application Form</h1>
+          <p class="helper-text">* denotes a required field</p>
+
+          <div class="field-row">
+            <div>
+              <label class="form-label" for="firstName">
+                First name<span>*</span>
+              </label>
+              <input
+                name="firstName"
+                type="text"
+                value={this.state.firstName}
+                onChange={this.handleInputChange}
+              />
+              <span class="message"></span>
+              <div style={{ fontSize: 8, color: "red" }}>
+                {this.state.firstNameError}
+              </div>
             </div>
-        )
+          </div>
+
+          <div class="field-row">
+            <label class="form-label cf" for="lastName">
+              Last name <span>*</span>
+            </label>
+            <input
+              name="lastName"
+              type="text"
+              value={this.state.lastName}
+              onChange={this.handleInputChange}
+            />
+            <span class="message"></span>
+          </div>
+
+          <div class="field-row">
+            <label class="form-label" for="email">
+              Email<span>*</span>
+            </label>
+            <input
+              name="email"
+              type="text"
+              value={this.state.email}
+              onChange={this.handleInputChange}
+            />
+            <span class="message"></span>
+          </div>
+          <div class="field-row">
+            <label class="form-label">Major</label>
+            <input
+              name="major"
+              type="text"
+              value={this.state.major}
+              onChange={this.handleInputChange}
+            />
+            <span class="message"></span>
+          </div>
+          <div class="field-row">
+            <label class="form-label">
+              Course<span>*</span>
+            </label>
+            <input
+              name="course"
+              type="text"
+              value={this.state.course}
+              onChange={this.handleInputChange}
+            />
+            <span class="message"></span>
+          </div>
+
+          <div class="field-row">
+            <label class="form-label">
+              Description<span>*</span>
+            </label>
+            <input
+              name="description"
+              type="text"
+              value={this.state.description}
+              onChange={this.handleInputChange}
+            />
+            <span class="message"></span>
+          </div>
+
+          <div class="field-row">
+            <label class="form-label">
+              Resume<span>*</span>
+            </label>
+            <input
+              name="resume"
+              type="file"
+              onChange={this.handleFileInputChange}
+            />
+          </div>
+          <div class="field-row">
+            <label class="form-label">
+              Upload Photo<span>*</span>
+              <input
+                name="photo"
+                type="file"
+                onChange={this.handleFileInputChange}
+              />
+            </label>
+          </div>
+
+          <div class="field-row">
+            <label class="form-label"></label>
+            <button type="submit" class="form-button">
+              Apply now
+            </button>
+          </div>
+        </form>
+      </>
+    );
+    {
+      /*
+      <div className="tutorForm">
+        <form className="formApply" onSubmit={this.handleSubmit}>
+          <div className="field-row">
+            <label>
+              First Name
+              <input
+                name="firstName"
+                type="text"
+                value={this.state.firstName}
+                onChange={this.handleInputChange}
+              />
+            </label>
+          </div>
+          <label>
+            Last Name
+            <input
+              name="lastName"
+              type="text"
+              value={this.state.lastName}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label>
+            Email
+            <input
+              name="email"
+              type="text"
+              value={this.state.email}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label>
+            Major
+            <input
+              name="major"
+              type="text"
+              value={this.state.major}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label>
+            Course
+            <input
+              name="course"
+              type="text"
+              value={this.state.course}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label>
+            Description
+            <input
+              name="description"
+              type="text"
+              value={this.state.description}
+              onChange={this.handleInputChange}
+            />
+          </label>
+          <label>
+            Upload Resume
+            <input
+              name="resume"
+              type="file"
+              onChange={this.handleFileInputChange}
+            />
+          </label>
+          <label>
+            Upload Photo
+            <input
+              name="photo"
+              type="file"
+              onChange={this.handleFileInputChange}
+            />
+          </label>
+          <button type="submit">Register</button>
+        </form>
+          </div>
+        */
     }
+  }
 }
 
-export default ApplyTutor; 
+export default ApplyTutor;
